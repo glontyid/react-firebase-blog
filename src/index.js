628bee1891ce0acb095f38b2b6db415330510c1b
Igor <glontyid@yandex.ru>
2021-04-15 23:01:23 +0400

    add app

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+import 'firebase/auth'
+import 'firebase/firestore'
+
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyA06wnl2Mdgdm-uHrSG_gG6wiw797UFf5k",
+        authDomain: "react-blog-7b6ce.firebaseapp.com",
+        projectId: "react-blog-7b6ce",
+        storageBucket: "react-blog-7b6ce.appspot.com",
+        messagingSenderId: "356829308813",
+        appId: "1:356829308813:web:612d09ca31303ee9157fc4",
+        measurementId: "G-TZFT6FGHBX"
+    }
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
